98349af4e96d1ac60a8e6719746a745993db1cd6
Alexander Larin <doremilarin@gmail.com>
2022-05-22 23:42:31 +0300

    added new and show pages

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const Post = require('./models/postschema');
 
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://localhost:27017/user-post');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
@@ -15,9 +16,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -25,8 +24,26 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/posts', (req, res) => {
-    res.render('posts/posts');
+// Show All Posts
+app.get('/posts', async (req, res) => {
+    const posts = await Post.find({});
+    res.render('posts/posts', { posts });
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts/new');
+});
+
+// Show chosen post
+app.get('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    res.render('posts/show', { post });
+});
+
+app.post('/posts', (req, res) => {
+    console.log(req.body);
+    res.send('New Posts are not available right now. Sorry...');
 });
 
 app.listen(3000, () => {
